a32ffc8a6be1956a360e8ac3def0d5476da2a015
Jarek Radosz <jradosz@gmail.com>
2022-03-23 22:34:21 +0100

    DEV: Add pluginId to modifyClass calls

--- javascripts/discourse/initializers/init-topic-excerpts.js
+++ javascripts/discourse/initializers/init-topic-excerpts.js
@@ -2,6 +2,7 @@ import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer("0.8", api => {
   api.modifyClass("component:topic-list-item", {
+    pluginId: "discourse-air",
     expandPinned: true
   })
 })
