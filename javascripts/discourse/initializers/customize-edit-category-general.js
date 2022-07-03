3ac9148232100543a61e0646c2720d11f9666771
Jarek Radosz <jradosz@gmail.com>
2022-07-03 19:28:55 +0200

    DEV: Add CI setup and fix linting issues (#18)

--- javascripts/discourse/initializers/customize-edit-category-general.js
+++ javascripts/discourse/initializers/customize-edit-category-general.js
@@ -1,26 +1,26 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
-import { cancel, later } from "@ember/runloop";
+import { cancel } from "@ember/runloop";
 
 export default {
   name: "customize-edit-category-general",
 
   initialize() {
-    withPluginApi("0.8.14", api => {
+    withPluginApi("0.8.14", (api) => {
       api.modifyClass("component:edit-category-general", {
         pluginId: "discourse-air",
 
         didInsertElement() {
           this._super(...arguments);
-          document.body.classList.add("edit-category")
+          document.body.classList.add("edit-category");
           this._focusCategoryName();
         },
 
         willDestroyElement() {
           this._super(...arguments);
-          document.body.classList.remove("edit-category")
+          document.body.classList.remove("edit-category");
           this._laterFocus && cancel(this._laterFocus);
         },
-      })
-    })
-  }
-}
+      });
+    });
+  },
+};
--- javascripts/discourse/initializers/init-topic-excerpts.js
+++ javascripts/discourse/initializers/init-topic-excerpts.js
@@ -1,8 +1,8 @@
 import { apiInitializer } from "discourse/lib/api";
 
-export default apiInitializer("0.8", api => {
+export default apiInitializer("0.8", (api) => {
   api.modifyClass("component:topic-list-item", {
     pluginId: "discourse-air",
-    expandPinned: true
-  })
-})
+    expandPinned: true,
+  });
+});
