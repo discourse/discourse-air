e9fc1fec2c7404a9064bb1f12916a176438da839
David Taylor <david@taylorhq.com>
2025-01-06 16:27:49 +0000

    DEV: Update for glimmer topic list

--- javascripts/discourse/initializers/init-topic-excerpts.js
+++ javascripts/discourse/initializers/init-topic-excerpts.js
@@ -1,8 +1,18 @@
 import { apiInitializer } from "discourse/lib/api";
+import { withSilencedDeprecations } from "discourse-common/lib/deprecated";
 
 export default apiInitializer("0.8", (api) => {
-  api.modifyClass("component:topic-list-item", {
-    pluginId: "discourse-air",
-    expandPinned: true,
-  });
+  const transformerExists = api.registerValueTransformer(
+    "topic-list-item-expand-pinned",
+    () => true
+  );
+
+  if (!transformerExists) {
+    withSilencedDeprecations("discourse.hbr-topic-list-overrides", () => {
+      api.modifyClass("component:topic-list-item", {
+        pluginId: "discourse-air",
+        expandPinned: true,
+      });
+    });
+  }
 });
