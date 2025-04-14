9090720ec1b9f140b5e13462e5a334ec67d93f99
David Taylor <david@taylorhq.com>
2025-04-14 12:24:58 +0100

    DEV: Remove legacy topic-list code

--- javascripts/discourse/initializers/init-topic-excerpts.js
+++ javascripts/discourse/initializers/init-topic-excerpts.js
@@ -1,13 +1,5 @@
 import { apiInitializer } from "discourse/lib/api";
-import { withSilencedDeprecations } from "discourse/lib/deprecated";
 
 export default apiInitializer("0.8", (api) => {
   api.registerValueTransformer("topic-list-item-expand-pinned", () => true);
-
-  withSilencedDeprecations("discourse.hbr-topic-list-overrides", () => {
-    api.modifyClass("component:topic-list-item", {
-      pluginId: "discourse-air",
-      expandPinned: true,
-    });
-  });
 });
