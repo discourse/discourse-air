58b7e299c2e79cfcc89e974f58413f91721a3adf
Jarek Radosz <jarek@cvx.dev>
2026-01-26 21:22:14 +0100

    DEV: Update linting and add types

--- spec/system/core_features_spec.rb
+++ spec/system/core_features_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe "Core features", type: :system do
+RSpec.describe "Core features" do
   before { upload_theme_or_component }
 
   it_behaves_like "having working core features"
